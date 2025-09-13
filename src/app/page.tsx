f7ee53638309b4aab71c366d499f239c8e5f4d44
devgsanches <guilhermesanches.pessoal@gmail.com>
2025-09-12 23:05:53 -0300

    feat: add title and description in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,22 @@ export default function Home() {
   return (
     <div className="w-screen h-screen bg-[#DCE9E2]">
       <Header />
+      <main className="px-6">
+        <div className="flex flex-col gap-4 items-center">
+          <p className="text-sm font-bold text-[#00856F]">
+            BOAS-VINDAS A DOCTORCARE 👋
+          </p>
+          <h2 className="text-4xl text-[#212529] text-center font-bold">
+            Assistência médica simplificada para todos
+          </h2>
+        </div>
+        <div className="mt-6 px-8">
+          <p className="text-lg text-[#495057] text-center">
+            Os médicos da DoctorCare vão além dos sintomas para tratar a causa
+            raiz de sua doença e proporcionar uma cura a longo prazo.
+          </p>
+        </div>
+      </main>
     </div>
   )
 }
