4d54acbe8625a82b79cfdff024b089290829df25
devgsanches <guilhermesanches.pessoal@gmail.com>
2025-09-12 22:47:49 -0300

    feat: add Header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,13 @@
+import { Menu } from 'lucide-react'
+import React from 'react'
+
+export function Header() {
+  return (
+    <header className="px-6 py-4 flex justify-between">
+      <h1 className="text-2xl text-[#212529]">
+        Doctor<span className="font-semibold text-[#00856F]">Care</span>
+      </h1>
+      <Menu color="#00856F" />
+    </header>
+  )
+}
